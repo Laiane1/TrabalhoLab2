7dd2627abcc4d22340770bb52958268da36276bc
Laiane1 <114609427+Laiane1@users.noreply.github.com>
2022-10-18 21:33:37 +0000

    foram tiradas variáveis desnecessárias

--- .ccls-cache/@home@runner@TrabalhoLab2/main.c
+++ .ccls-cache/@home@runner@TrabalhoLab2/main.c
@@ -3,80 +3,75 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
- 
+int main() {
 
-int jogador, computador, pedra, papel, tesoura, saida=4;
+  int jogador, computador, saida = 4;
 
-  
-  while(saida==4){
-  printf("Escolha uma opção:\n 1- pedra 2- papel 3- tesoura\n ");
-  scanf("%d", &jogador);
-  
+  while (saida == 4) {
+    printf("Escolha uma opção:\n 1- pedra 2- papel 3- tesoura\n ");
+    scanf("%d", &jogador);
+
     if (jogador == computador)
-        printf("\nEmpate\n");
-  
- 
-    if (jogador == 1 && computador == 2){
-        printf("\nVocê perdeu\n");
-      }
+      printf("\nEmpate\n");
 
-      else if(jogador == 2 && computador == 1){
-        printf("\nVocê venceu\n");
-      }
-
-  if(jogador == 1 && computador == 3){
-    printf("\nVocê venceu\n");
-  }
-
-  else if(jogador == 3 && computador == 1){
-    printf("\nVocê perdeu\n");
-  }
-
-  if(jogador == 2 && computador == 3){
-    printf("\nVocê perdeu\n");
-  }
-
-  else if(jogador == 3 && computador == 2){
-    printf("\nVocê ganhou\n");
-  }
- 
-          
-    srand(time(NULL));
- 
-    computador = rand()%3+1;
-  printf("Escolha do computador: %d", computador);
-
-   if (jogador == computador)
-        printf("\nEmpate\n");
-  
-    if (jogador == 1 && computador == 2){
-        printf("\nVocê perdeu\n");
-      }
-
-      else if(jogador == 2 && computador == 1){
-        printf("\nVocê venceu\n");
-      }
-
-  if(jogador == 1 && computador == 3){
-    printf("\nVocê venceu\n");
-  }
-
-  else if(jogador == 3 && computador == 1){
-    printf("\nVocê perdeu\n");
-  }
-
-  if(jogador == 2 && computador == 3){
-    printf("\nVocê perdeu\n");
-  }
-
-  else if(jogador == 3 && computador == 2){
-    printf("\nVocê ganhou\n");
-  }
-    printf("Deseja iniciar uma nova partida? Digite 4 para sim e qualquer outro número para não: ");
-    scanf("%d", &saida);
-
-    
+    if (jogador == 1 && computador == 2) {
+      printf("\nVocê perdeu\n");
     }
-    return 0;
+
+    else if (jogador == 2 && computador == 1) {
+      printf("\nVocê venceu\n");
+    }
+
+    if (jogador == 1 && computador == 3) {
+      printf("\nVocê venceu\n");
+    }
+
+    else if (jogador == 3 && computador == 1) {
+      printf("\nVocê perdeu\n");
+    }
+
+    if (jogador == 2 && computador == 3) {
+      printf("\nVocê perdeu\n");
+    }
+
+    else if (jogador == 3 && computador == 2) {
+      printf("\nVocê ganhou\n");
+    }
+
+    srand(time(NULL));
+
+    computador = rand() % 3 + 1;
+    printf("Escolha do computador: %d", computador);
+
+    if (jogador == computador)
+      printf("\nEmpate\n");
+
+    if (jogador == 1 && computador == 2) {
+      printf("\nVocê perdeu\n");
+    }
+
+    else if (jogador == 2 && computador == 1) {
+      printf("\nVocê venceu\n");
+    }
+
+    if (jogador == 1 && computador == 3) {
+      printf("\nVocê venceu\n");
+    }
+
+    else if (jogador == 3 && computador == 1) {
+      printf("\nVocê perdeu\n");
+    }
+
+    if (jogador == 2 && computador == 3) {
+      printf("\nVocê perdeu\n");
+    }
+
+    else if (jogador == 3 && computador == 2) {
+      printf("\nVocê ganhou\n");
+    }
+    printf("Deseja iniciar uma nova partida? Digite 4 para sim e qualquer "
+           "outro número para não: ");
+    scanf("%d", &saida);
+  }
+  return 0;
 }
